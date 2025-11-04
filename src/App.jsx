ef996a461e14eef7a4ffb9e49dee4b710b6a80bd
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2025-11-04 10:05:25 +0200

    adding selecting desserts function and UI, saving items into localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import { useState, useEffect } from 'react';
 import './App.css'
 import Header from './components/Header.jsx';
-import AllDesserts from './components/AllDesserts.jsx';
+import DessertsList from './components/DessertsList.jsx';
 import { ALL_DESSERTS } from './data.js';
 
 function App() {
-  
+  const [selectedDesserts, setSelectedDesserts] = useState(() => {
+    const saved = localStorage.getItem('selectedDesserts');
+    return saved ? JSON.parse(saved) : []
+  });
+
+  useEffect(() => {
+    localStorage.setItem('selectedDesserts', JSON.stringify(selectedDesserts));
+  }, [selectedDesserts]);
+
+  function handleSelectDessert(id) {
+    const dessert = ALL_DESSERTS.find((item) => item.id === id);
+    if (!dessert) return; 
+    const alreadySelected = selectedDesserts.some((d) => d.id === id);
+    if (alreadySelected) return;
+    setSelectedDesserts((prev) => [...prev, dessert]);
+  }
+
   return (
     <>
       <Header />
-      <AllDesserts desserts={ALL_DESSERTS} />
+      <DessertsList 
+        desserts={selectedDesserts} 
+        selectDessert={handleSelectDessert} 
+        titleText="Selected deserts"
+      />
+      <DessertsList 
+        desserts={ALL_DESSERTS} 
+        selectDessert={handleSelectDessert}
+        titleText="All our desserts!" 
+      />
     </>
   )
 }
--- src/components/DessertsList.jsx
+++ src/components/DessertsList.jsx
@@ -1,13 +1,14 @@
-export default function AllDesserts({ desserts }) {
+export default function DessertsList({ titleText, desserts, selectDessert }) {
   return (
-    <section className="border border-rose-400 p-6 rounded-2xl">
-      <h3 className="text-rose-400 text-2xl mb-6">All our desserts!</h3>
-
+    <section className="p-6 mb-6">
+      <h3 className="mb-6">{titleText}</h3>
+      {!desserts.length && <p className="text-rose-200">Ooops... Nothing to show here</p>}
       <ul className="flex flex-wrap -mx-2">
         {desserts.map((dessert) => (
           <li
             key={dessert.id}
             className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4 box-border"
+            onClick={() => selectDessert(dessert.id)}
           >
             <div className="relative rounded-xl overflow-hidden bg-[#1b2033] transition-transform duration-300 ease-in-out hover:rotate-[10deg] hover:shadow-[0_0_20px_5px_rgba(255,255,100,0.6)] cursor-pointer">
               <img
@@ -15,8 +16,6 @@ export default function AllDesserts({ desserts }) {
                 alt={dessert.image.alt}
                 className="block w-full h-auto object-cover"
               />
-
-              {/* Title overlay */}
               <div className="absolute bottom-2 left-2 bg-black/60 border border-rose-400 text-rose-200 text-sm px-3 py-1 rounded-md backdrop-blur-sm">
                 {dessert.title}
               </div>
